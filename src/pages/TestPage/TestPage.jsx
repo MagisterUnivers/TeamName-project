e06ccb8117bd44197e43c4a40356bf4d0ecd2acc
Irgey <116591589+Irgey@users.noreply.github.com>
2023-07-11 17:48:36 +0200

    footer nav complete

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,8 +1,14 @@
 import FollowUs from 'components/FollowUs/FollowUs';
+import { Nav } from 'components/Nav/Nav';
 import React from 'react';
 
 const TestPage = () => {
-  return <FollowUs />;
+  return (
+    <>
+      <Nav />
+      <FollowUs />
+    </>
+  );
 };
 
 export default TestPage;
