d66030e1621465b7bcd5e3f90acc6f04ef0dfed8
Tanya <tkravche@yahoo.com>
2023-07-13 23:32:59 +0200

    all for drinksSearch

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,25 +1,5 @@
-import DrinksList from '../../components/DrinksList/DrinksList';
-import DrinksSearch from 'components/DrinksSearch/DrinksSearch';
-import Paginator from 'components/Paginator/Paginator';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getCocktailsByCategoryThunk } from 'redux/Cocktails/cocktailsOperations';
-
 const TestPage = () => {
-  const dispatch = useDispatch();
-  const category = encodeURIComponent('Cocktail');
-  useEffect(() => {
-    dispatch(getCocktailsByCategoryThunk(category));
-  }, [dispatch, category]);
-
-  return (
-    <>
-      <h1>Drinks</h1>
-      <DrinksSearch />
-      <DrinksList />
-      <Paginator />
-    </>
-  );
+  return <></>;
 };
 
 export default TestPage;
--- src/redux/Cocktails/cocktailsOperations.js
+++ src/redux/Cocktails/cocktailsOperations.js
@@ -1,14 +1,98 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { instance } from 'redux/Auth/authOperations';
 
-export const getCocktailsByCategoryThunk = createAsyncThunk(
-  '@@cocktails/category',
-  async (category, { rejectWithValue }) => {
+// Cocktails
+
+export const getCategoriesListThunk = createAsyncThunk(
+  '@@cocktails/categoriesList',
+  async (_, { rejectWithValue }) => {
     try {
-      const res = await instance.get('api/recipes/' + category);
+      const res = await instance.get('api/recipes/category-list');
       return res.data;
     } catch (error) {
       return rejectWithValue(error.response.status);
     }
   }
 );
+
+export const getCocktailsByCategoryThunk = createAsyncThunk(
+  '@@cocktails/byCategory',
+  async (category, { rejectWithValue }) => {
+    try {
+      const res = await instance.get(
+        `api/recipes/${encodeURIComponent(category)}`
+      );
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.status);
+    }
+  }
+);
+
+export const getCocktailByIdThunk = createAsyncThunk(
+  '@@cocktails/byId',
+  async (id, { rejectWithValue }) => {
+    try {
+      const res = await instance.get(
+        `api/recipes/id/${id}`
+      );
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.status);
+    }
+  }
+);
+
+// Search
+
+export const searchAllDrinksThunk = createAsyncThunk(
+  '@@cocktails/search',
+  async (search, { rejectWithValue }) => {
+    try {
+      const params = {};
+      search.chosenCategory && (params.category = search.chosenCategory);
+      search.chosenIngredient && (params.ingredient = search.chosenIngredient);
+      search.query && (params.query = search.query);
+      const res = await instance.get('api/search', {
+        params,
+      });
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.status);
+    }
+  }
+);
+
+//Ingredients
+
+export const getIngredientsListThunk = createAsyncThunk(
+  '@@cocktails/ingredientsList',
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await instance.get('api/ingredients/list');
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.status);
+    }
+  }
+);
+
+//Glasses
+
+export const getAllGlassesThunk = createAsyncThunk(
+  '@@cocktails/glassesList',
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await instance.get('api/glass');
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.status);
+    }
+  }
+);
+
+// Own
+
+// Favorites
+
+// Popular
