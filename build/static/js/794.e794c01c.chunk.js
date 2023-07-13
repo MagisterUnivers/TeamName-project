015de70a4bda3f5b30f879f3a548e655387a0519
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-13 11:48:07 +0300

    REVENT

---
+++ build/static/js/794.e794c01c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);r(2791);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"NotFoundPage"})}}}]);
+//# sourceMappingURL=794.e794c01c.chunk.js.map
