4f5457ab3551f1457d32accd9ec1d5000d3919f3
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-15 13:24:15 +0300

    FIX: ToggleTheme reducer + ThemeButton component

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -1,27 +1,30 @@
-import { useDispatch, useSelector } from "react-redux"
-import { StyledButton, StyledMoonIcon, StyledSunIcon } from "./ThemeButton.styled"
-import { selectTheme } from "redux/selectors"
-import { updateThemeThunk } from "redux/UserInfo/userOperations";
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  StyledButton,
+  StyledMoonIcon,
+  StyledSunIcon,
+} from './ThemeButton.styled';
+import { selectTheme } from 'redux/selectors';
+import { updateThemeThunk } from 'redux/UserInfo/userOperations';
 
 export const ThemeButton = ({ isDarkTheme, setIsDarkTheme }) => {
   const dispatch = useDispatch();
-  const theme = useSelector(selectTheme)
-  console.log(theme);
+  const themeSelector = useSelector(selectTheme);
   const toggleTheme = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    const newTheme = themeSelector === 'dark' ? 'light' : 'dark';
     const payload = JSON.stringify({ theme: newTheme });
-        dispatch(updateThemeThunk(payload))
-    }
+    dispatch(updateThemeThunk(payload));
+  };
   return (
     <StyledButton
       title={isDarkTheme ? 'switch to light theme' : 'switch to dark theme'}
       onClick={toggleTheme}
     >
-      {theme === 'dark' ? (
+      {themeSelector === 'dark' ? (
         <StyledSunIcon title="switch to light theme" />
       ) : (
         <StyledMoonIcon title="switch to dark theme" />
       )}
     </StyledButton>
   );
-}
+};
--- src/redux/UserInfo/userInfoSlice.js
+++ src/redux/UserInfo/userInfoSlice.js
@@ -1,36 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { updateThemeThunk } from "./userOperations";
+import { updateThemeThunk } from './userOperations';
 
 const initialState = {
-    theme: 'dark',
-}
+  theme: 'dark',
+};
 
 const userInfoSlice = createSlice({
-    name: '@@userInfo',
-    initialState,
-    reducers: {
-        setTheme: state => {
-            state.theme = state.theme === 'dark' ? 'light' : 'dark';
-        }
-    }, extraReducers: {
-        [updateThemeThunk.pending]: (state, { payload }) => {
-            state.loading = true;
-            Loading.hourglass('We are validating your data...');
-        },
-        [updateThemeThunk.fulfilled]: (state, { payload }) => {
-            state.theme = payload;
-            state.loading = false;
-            Loading.remove();
-        },
-        [updateThemeThunk.rejected]: (state, { payload }) => {
-            state.error = payload;
-            // state.loading = false;
-            Loading.remove();
-        },
-    }
-}
-)
+  name: '@@userInfo',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [updateThemeThunk.pending]: (state, { payload }) => {
+      state.loading = true;
+      Loading.hourglass('We are validating your data...');
+    },
+    [updateThemeThunk.fulfilled]: (state, { payload }) => {
+      // crerate algorithm for toggleTheme
+      // create less fetch for theme
+
+      state.theme = payload.theme;
+      state.loading = false;
+      Loading.remove();
+    },
+    [updateThemeThunk.rejected]: (state, { payload }) => {
+      state.error = payload;
+      // state.loading = false;
+      Loading.remove();
+    },
+  },
+});
 
 export const userInfoReducer = userInfoSlice.reducer;
 export const { setTheme } = userInfoSlice.actions;
