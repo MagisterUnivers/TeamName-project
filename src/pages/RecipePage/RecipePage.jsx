c5f5282cd51cd828b750b190f254b80c6fb6153a
Tanya <tkravche@yahoo.com>
2023-07-17 14:18:31 +0200

    commented RecipePage

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,22 +1,27 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router';
+// import React, { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { useParams } from 'react-router';
+// import { useSelector } from 'react-redux';
 
-import { getCocktailByIdThunk } from 'redux/Cocktails/cocktailsOperations';
-import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
+// import { getCocktailByIdThunk } from 'redux/Cocktails/cocktailsOperations';
+// import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
+// import { selectCocktails } from 'redux/selectors';
+// import DrinkCard from 'components/DrinkCard/DrinkCard';
 
-const RecipePage = () => {
-  const { drinkId } = useParams();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getCocktailByIdThunk(drinkId));
-  }, [dispatch, drinkId]);
+// const RecipePage = () => {
+//   const { drinkId } = useParams();
+//   const dispatch = useDispatch();
+//   const cocktail = useSelector(selectCocktails);
+//   useEffect(() => {
+//     dispatch(getCocktailByIdThunk(drinkId));
+//   }, [dispatch, drinkId]);
 
-  return (
-    <>
-      <MainPageTitle title={'A Drink'} />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <MainPageTitle title={'A Drink'} />
+//       <DrinkCard id={drinkId} cocktail={cocktail} />
+//     </>
+//   );
+// };
 
-export default RecipePage;
+// export default RecipePage;
