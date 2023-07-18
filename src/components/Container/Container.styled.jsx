be0829928a9b4bcb0012ef5469dd0fb10af51ef9
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-18 14:25:13 +0300

    ADD: PT - Container 1440px

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -18,6 +18,7 @@ export const StyledContainer = styled.div`
 
   @media screen and (min-width: 1440px) {
     width: 1440px;
+    padding-top: 244px;
     padding-left: 100px;
     padding-right: 100px;
   }
