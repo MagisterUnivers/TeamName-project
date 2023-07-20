e4cad856424a32ee7347c86d6e00a992a3e58f3a
Irgey <116591589+Irgey@users.noreply.github.com>
2023-07-20 15:06:02 +0200

    footer complete

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -4,22 +4,17 @@ import { devices } from 'constants/breakpoints';
 export const StyledContainer = styled.div`
   width: 100%;
   margin: 0 auto;
-  padding-top: 152px;
   padding-left: 20px;
   padding-right: 20px;
-  padding-bottom: 80px;
 
   @media ${devices.tablet} {
     width: 768px;
-    padding-top: 224px;
     padding-left: 32px;
     padding-right: 32px;
-    padding-bottom: 140px;
   }
 
   @media ${devices.desktop} {
     width: 1440px;
-    padding-top: 244px;
     padding-left: 100px;
     padding-right: 100px;
   }
