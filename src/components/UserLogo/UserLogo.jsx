76abee8e362ad4f4d36fe3b1c286c9a6a4f935dc
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-20 10:56:11 +0300

    FIX: Build

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,10 +1,4 @@
-import React, { useState } from 'react';
-import {
-  NameStyled,
-  UserAvatar,
-  UserAvatarWrp,
-  UserLogoWrp,
-} from './UserLogoStyled';
+import { useState } from 'react';
 // import { UserLogoModal } from 'components';
 // {тимчасово підключила свою модалку, з логікою виконання. Олена};
 // import EditProfileModal from 'components/Modals/UserInfoModal/EditProfileModal';
@@ -17,16 +11,25 @@ import {
 import { UserLogoModal } from 'components';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'redux/selectors';
+import {
+  NameStyled,
+  UserAvatar,
+  UserAvatarWrp,
+  UserLogoWrp,
+} from './UserLogoStyled';
 
 export const UserLogo = () => {
   const [showUserLogoModal, setShowUserLogoModal] = useState(false);
   const user = useSelector(selectUser);
 
   return (
-    <UserLogoWrp onClick={()=> setShowUserLogoModal(!showUserLogoModal)}>
-       <UserAvatarWrp>
-       <UserAvatar src={user?.avatar || require('../../assets/img/useravatar_plug.png')} alt="avatar" />
-       </UserAvatarWrp>
+    <UserLogoWrp onClick={() => setShowUserLogoModal(!showUserLogoModal)}>
+      <UserAvatarWrp>
+        <UserAvatar
+          src={user?.avatar || require('../../assets/img/useravatar_plug.png')}
+          alt="avatar"
+        />
+      </UserAvatarWrp>
       <NameStyled>{user?.name || 'no Name'}</NameStyled>
       {showUserLogoModal && <UserLogoModal />}
     </UserLogoWrp>
