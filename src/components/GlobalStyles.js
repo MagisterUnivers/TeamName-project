661ecaa82d3e212065810538b315b6feb580d811
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-07-22 21:04:35 +0300

    some fixes

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -29,7 +29,7 @@ body {
   background-color: ${props => props.theme.bgrColor};
   transition: background-color 1s;
 
-  /* background-image: radial-gradient(
+  background-image: radial-gradient(
     circle at center,
     white 0%,
     #BCE6D2 40%
@@ -58,17 +58,24 @@ body {
     white 0%,
     #4070CD 40%
   );
-  background-size: 280px 250px, 250px 250px, 200px 200px, 200px 400px,400px 400px, 200px 120px, 200px 120px;
+  background-size: 280px 250px, 
+  250px 250px, 
+  200px 200px, 
+  200px 400px, 
+  400px 400px, 
+  200px 120px, 
+  200px 120px;
   background-repeat: no-repeat;
   
+  backdrop-filter: blur(300px);
+
   background-position: right -200px top 26%,
+  right -200px top 20%,
   right -100px top 20%,
   left -40px top 10%,
   left -10% top 5%,
   left -395px  bottom 0%,
   left 70%  bottom -119px;
-  backdrop-filter: blur(200px);
-
  
   @media ${devices.tablet} {
  background-position: right -270px top 26%,
@@ -78,17 +85,17 @@ body {
  left -80% bottom 1%,
  left 50%  bottom -100px,
  right 10px bottom -100px;
-  }
+}
 
   @media ${devices.desktop} {
    background-position: right -270px top 26%,
    right 70px top 18%,
-   left 20% top 10%,
-   left -5% top 10%,
-   left -26% bottom 1%,
+   left 15% top 7%,
+   left -11% top 10%,
+   left -26% bottom 3%,
    left 50%  bottom -100px,
    right 10px bottom -100px;
-  } */
+  }
 }
 
 h1,
