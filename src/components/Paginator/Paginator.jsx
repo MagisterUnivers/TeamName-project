551c66cfca445dc72f66f4736d40625cbbe481c1
KsenyaKoss <roxenn34@gmail.com>
2023-07-24 09:26:39 +0300

    ADDED: pagination router

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,19 +1,24 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Pagination } from '@mui/material';
-import { selectPage, selectTotalHits } from 'redux/selectors';
+import { Pagination, PaginationItem } from '@mui/material';
 import { setPage } from 'redux/Cocktails/cocktailsSlice';
-import { Wrapper, WrapperGeneralStyled } from './Paginator.styled';
+import { useMediaRules } from '../../hooks/useMediaRules';
+import { selectPage, selectTheme, selectTotalHits } from 'redux/selectors';
+import { Link, useLocation} from 'react-router-dom';
 
 export const Paginator = () => {
   let page = useSelector(selectPage);
   const dispatch = useDispatch();
+  const { isMobile, isTablet, isDesktop } = useMediaRules();
   const totalHits = useSelector(selectTotalHits);
-  const limit = 8; //!!!!
-  const pageQuantity = Math.ceil(totalHits / limit);
-  // console.log(pageQuantity);
+  const limit = isDesktop ? 9 : 8;
+  const pageQuantity = Math.floor(totalHits / limit);
+  const theme = useSelector(selectTheme);
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
-    <WrapperGeneralStyled>
-      {pageQuantity > 1 && (
+    <>
+      {!!pageQuantity  && (
         <Pagination
           count={pageQuantity}
           page={page}
@@ -21,9 +26,35 @@ export const Paginator = () => {
             window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
           }}
           onChange={(_, num) => dispatch(setPage(num))}
-          sx={{ marginY: 3, marginX: 'auto' }}
+          renderItem={
+            (item) => (
+               <PaginationItem
+               component={Link}
+               to={`${currentPath}?page=${item.page}`}
+               {...item}/>
+            )
+          }
+          sx={{
+            marginTop: isMobile ? '40px' : isTablet ? '80px' : '118px',
+            marginBottom: isMobile? "80px" : "140px",
+            marginX: 'auto',
+            maxWidth: isMobile ? '295px' : '436px',
+            maxHeight: '55px',
+            '& .MuiPaginationItem-root': {
+              fontSize: '12px',
+              fontWeight: '500',
+              color: theme === 'dark' ? 'var(--main-text-color)' : "'#0A0A11'",
+              transition: 'background 0.3s ease',
+              '&:hover': {
+                background: 'rgba(64, 112, 205, 0.4)',
+              },
+            },
+          }}
         />
       )}
-    </WrapperGeneralStyled>
+    </>
   );
 };
+
+export default Paginator;
+
--- src/components/Paginator/Paginator.styled.js
+++ src/components/Paginator/Paginator.styled.js
@@ -1,70 +1,17 @@
-import { Pagination } from '@mui/material';
 import styled from 'styled-components';
 import { devices } from 'constants/breakpoints';
 
 export const WrapperGeneralStyled = styled.div`
-display:flex;
-justify-content:center;
-   margin-top: 40px;
+  display: flex;
+  margin-top: 40px;
   margin-bottom: 80px;
+
   @media ${devices.tablet} {
     margin-top: 80px;
-  margin-bottom: 140px;
+    margin-bottom: 140px;
   }
 
   @media ${devices.desktop} {
-  }
-  &::placeholder {
-    color: #f3f3f3;
-  }`;
-
-export const Wrapper = styled(Pagination)`
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-  
-//arrows
-  & .MuiPaginationItem-icon {
-    fill: rgba(243, 243, 243, 0.30);;
-  }
-
-  & .MuiPaginationItem-ellipsis {
-    color: green;
-  }
-//dots
-  & .MuiPagination-ul {
-    display: flex;
-    flex-wrap: nowrap;
-    max-width: 100%;
-
-    margin-top: 0;
-    gap: 0px;
-    color: #F3F3F3;
-  }
-//numbers
-  & .MuiPaginationItem-page {
-    padding: 0;
-    /* min-width: 27px; */
-    color: #F3F3F3;
-  }
-
-  & .MuiPaginationItem-page.Mui-selected {
-    /* width: 27px;
-    height: 27px; */
-
-    background-color: purple;
-    color: green;
-
-    /* transition: var (--transition-function); */
-
-    &:hover,
-    &:focus {
-      color: blue;
-
-      background-color: red;
-    }
-
+    margin-top: 118px;
   }
 `;
