06a758a063732a287cc6caa97bd9d648d2790d4d
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-07-24 11:42:15 +0300

    add empty favorite and fix styles

--- src/components/FavoriteList/FavoriteList.styled.js
+++ src/components/FavoriteList/FavoriteList.styled.js
@@ -1,14 +1,6 @@
 import styled from 'styled-components';
 import { devices } from '../../constants/breakpoints';
 
-export const Section = styled.section`
-  max-width: 100%;
-  margin-bottom: 40px;
-  overflow: hidden;
-  @media ${devices.tablet} {
-    margin-bottom: 80px;
-  }
-`;
 export const FavoriteListStyled = styled.ul`
   display: grid;
   row-gap: 28px;
@@ -22,5 +14,50 @@ export const FavoriteListStyled = styled.ul`
     grid-template-columns: repeat(3, 1fr);
     column-gap: 20px;
     row-gap: 80px;
+  }
+`;
+
+export const NotFavorioteText = styled.p`
+  width: 205px;
+  color: ${props => props.theme.color};
+  text-align: center;
+  font-family: Manrope;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+  @media ${devices.tablet} {
+    font-size: 16px;
+    line-height: 22px;
+  }
+`;
+
+export const WrapDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const notFoundImg1x = require('../../assets/img/NotFound1x.jpg');
+const notFoundImg2x = require('../../assets/img/NotFound2x.jpg');
+const notFoundWhite1x = require('../../assets/img/notFoundWhite1x.png');
+const notFoundWhite2x = require('../../assets/img/notFoundWhite2x.png');
+
+export const NotFoundImg = styled.div`
+  width: 375px;
+  height: 517px;
+  background-image: ${props =>
+    props.theme === 'dark'
+      ? props.ismobile
+        ? `url(${notFoundImg2x})`
+        : `url(${notFoundImg1x})`
+      : props.isMobile
+      ? `url(${notFoundWhite2x})`
+      : `url(${notFoundWhite1x})`};
+  background-repeat: no-repeat;
+  background-position: center;
+  @media ${devices.tablet} {
+    width: 465px;
   }
 `;
--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,18 +1,19 @@
 import { useDispatch } from 'react-redux';
 import { getAllFavoriteDrinksThunk } from 'redux/Cocktails/cocktailsOperations';
-import { Paginator, MainPageTitle } from 'components';
+import { Paginator, MainPageTitle, Section } from 'components';
 import { FavoriteList } from 'components/FavoriteList/FavoriteList';
+import { PageWrapFavorite } from './FavoritePage.styled';
 
 const FavoritePage = () => {
   const dispatch = useDispatch();
   dispatch(getAllFavoriteDrinksThunk());
 
   return (
-    <>
+    <PageWrapFavorite>
       <MainPageTitle title={'Favorites'} />
       <FavoriteList />
       <Paginator />
-    </>
+    </PageWrapFavorite>
   );
 };
 
