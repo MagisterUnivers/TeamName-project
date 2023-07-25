252aeb78e407d99dc6e3b404b2bb827e02509e37
Irgey <116591589+Irgey@users.noreply.github.com>
2023-07-25 15:32:18 +0200

    Update BurgerMenu.jsx

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,19 +1,22 @@
 import { NameStyled } from 'components/UserLogo/UserLogoStyled';
 import { Navigation, ThemeButton } from 'components';
-import { useMediaRules } from 'hooks';
+import { useMediaRules } from 'hooks/useMediaRules';
 import { BurgerMenuContainer } from './BurgerMenu.styled';
 
-export const BurgerMenu = ({ showBurgerMenu, toggleFn }) => {
+export const BurgerMenu = ({ isBurgerMenuShown, toggleFn }) => {
   const { isMobile } = useMediaRules();
   return (
-    <BurgerMenuContainer showBurgerMenu={showBurgerMenu}>
-      <Navigation onClick={toggleFn} />
-      {isMobile && (
-        <div className="themeWrp">
-          <NameStyled>Switch Theme: </NameStyled>
-          <ThemeButton externalFn={toggleFn} />
-        </div>
-      )}
-    </BurgerMenuContainer>
+    <>
+      {/* Решта вашого коду для компонентів, якщо вони є */}
+      <BurgerMenuContainer showBurgerMenu={isBurgerMenuShown}>
+        <Navigation onClick={toggleFn} />
+        {isMobile && (
+          <div className="themeWrp">
+            <NameStyled>Перемкнути тему: </NameStyled>
+            <ThemeButton externalFn={toggleFn} />
+          </div>
+        )}
+      </BurgerMenuContainer>
+    </>
   );
 };
