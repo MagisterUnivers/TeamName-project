907784ce08211911250da2cc8bc75bf284928e02
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-25 14:42:09 +0300

    2

--- src/components/BurgerMenu/Button/Button.jsx
+++ src/components/BurgerMenu/Button/Button.jsx
@@ -1,8 +1,9 @@
-import { BurgerMenuButtonStyled, getStyledIcon } from './Button.styled';
 import { ReactComponent as iconClose } from '../../../assets/icons/close.svg';
 import { ReactComponent as iconBurger } from '../../../assets/icons/burger-menu.svg';
+import { BurgerMenuButtonStyled, getStyledIcon } from './Button.styled';
 const StyledIconClose = getStyledIcon(iconClose);
 const StyledIconBurger = getStyledIcon(iconBurger);
+
 export const BurgerMenuButton = ({ showBurgerMenu, onClose }) => {
   return (
     <BurgerMenuButtonStyled onClick={onClose}>
