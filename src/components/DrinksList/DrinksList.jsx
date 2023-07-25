21eb6bc28121934e7f3e2b80876b5fdca9fd9db1
Tanya <tkravche@yahoo.com>
2023-07-25 18:24:45 +0200

    deletion of duplicates

--- src/components/DrinksList/DrinksList.jsx
+++ src/components/DrinksList/DrinksList.jsx
@@ -1,31 +1,12 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectPage, selectSearch, selectSearchResults } from 'redux/selectors';
-import { searchAllDrinksThunk } from 'redux/Cocktails/cocktailsOperations';
+import { useSelector } from 'react-redux';
+import { selectSearchResults } from 'redux/selectors';
 import { DrinkCard, NotFound } from 'components';
-import { useNavigate } from 'react-router';
-import { useMediaRules } from 'hooks';
+
 import { DrinkListStyled, Section } from './DrinkList.styled';
 
 export const DrinksList = () => {
-  const dispatch = useDispatch();
-  const { isDesktop } = useMediaRules();
   const searchResults = useSelector(selectSearchResults);
-  const page = useSelector(selectPage);
-  const search = useSelector(selectSearch);
-  const navigate = useNavigate();
-  const limit = isDesktop ? 9 : 8;
-
-  useEffect(() => {
-    dispatch(searchAllDrinksThunk({ search, page, limit }));
-    navigate(
-      `/main/drinks/${encodeURIComponent(
-        search.chosenCategory
-      )}?query=${encodeURIComponent(search.query)}&ingredient=${
-        search.chosenIngredient
-      }&page=${page}`
-    );
-  }, [dispatch, search, page, limit]); //eslint-disable-line
 
   useEffect(() => {
     window.scrollTo(0, 0);
