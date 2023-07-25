22a6c2e5431776f467b273ba9a4d19e57b430b3f
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-07-25 11:26:16 +0300

    FIX: Paginator selected page

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Box } from '@mui/material';
-import Pagination  from '@mui/material/Pagination'
+import Pagination from '@mui/material/Pagination';
 import { setPage } from 'redux/Cocktails/cocktailsSlice';
 import { useMediaRules } from '../../hooks/useMediaRules';
 import { selectPage, selectTheme, selectTotalHits } from 'redux/selectors';
@@ -33,7 +33,7 @@ export const Paginator = () => {
           boundaryCount={1}
           siblingCount={1}
           onClick={() => {
-           window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
           }}
           sx={{
             marginX: 'auto',
@@ -42,17 +42,20 @@ export const Paginator = () => {
               fontWeight: '500',
               color: theme === 'dark' ? 'var(--main-text-color)' : "'#0A0A11'",
               transition: 'background 0.3s ease',
-              '&:hover, &:focus': {
+              '&:hover': {
                 background: 'rgba(64, 112, 205, 0.4)',
               },
             },
-            '& .MuiPaginationItem-page': {
-              color: theme === 'dark' ? 'var(--main-text-color)' : "'#0A0A11'",
-              background: 'transparent',
-              transition: 'background-color 500ms ease',
-              '&:hover, &:focus ': {
-                background: 'rgba(64, 112, 205, 0.5)',
-              },
+            // '& .MuiPaginationItem-page': {
+            //   color: theme === 'dark' ? 'var(--main-text-color)' : "'#0A0A11'",
+            //   background: 'transparent',
+            //   transition: 'background-color 500ms ease',
+            //   '&:hover, &:focus ': {
+            //     background: 'rgba(64, 112, 205, 0.5)',
+            //   },
+            // },
+            '& .MuiPaginationItem-page.Mui-selected': {
+              background: 'rgba(64, 112, 205, 0.4)',
             },
             'Ul.MuiPagination-ul': {
               flexWrap: 'nowrap',
