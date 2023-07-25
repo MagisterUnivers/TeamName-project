febe9bf4603deb47f988a28414eb7bd03dcf463c
MagisterUnivers <36455862+MagisterUnivers@users.noreply.github.com>
2023-07-25 23:22:06 +0000

    Deploying to gh-pages from @ MagisterUnivers/TeamName-project@b86d1ea1bdf4e3efef540c93cecea5c2c93fc271 🚀

--- static/js/123.39a4f6b9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[123],{123:function(e,t,r){r.r(t);r(791);var c=r(184);t.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"Welcome Page"}),(0,c.jsx)("p",{children:"Text test"})]})}}}]);
-//# sourceMappingURL=123.39a4f6b9.chunk.js.map
